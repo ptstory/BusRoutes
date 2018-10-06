6626cb420182d53d8929d5be6a2256196401cf52
Perry Story <perrystory@Perrys-MBP.gsu.edu>
2018-10-06 19:55:07 -0400

    add progress bars

--- screens/AchievementsScreen.js
+++ screens/AchievementsScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { TouchableOpacity, Text, View, StyleSheet, Image, Button } from "react-native";
+import * as Progress from 'react-native-progress';
 
 import Header from "../components/Header";
 
@@ -7,17 +8,60 @@ class AchievementsScreen extends React.Component {
   static navigationOptions = {
     title: "Achievements"
   };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      progress: 0,
+      indeterminate: true,
+    };
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  animate() {
+    let progress = 0;
+    this.setState({ progress });
+    setTimeout(() => {
+      this.setState({ indeterminate: false });
+      setInterval(() => {
+        progress += Math.random() / 5;
+        // if (progress > 1) {
+        //   progress = 1;
+        // }
+        this.setState({ progress });
+      }, 500);
+    }, 1500);
+  }
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
         <Header navigation={this.props.navigation} />
-        <Text>Progress Bar </Text>
-        <Text>Progress Bar </Text>
-        <Text>Progress Bar </Text>
+
+        <Image style={styles.icon} source={require("../assets/bus.jpg")} />
+        <Progress.Bar
+          style={styles.progress}
+          progress={this.state.progress}
+          indeterminate={this.state.indeterminate}
+        />
+        <Image style={styles.icon} source={require("../assets/stoplight.jpg")} />
+        <Progress.Bar
+          style={styles.progress}
+          progress={this.state.progress}
+          indeterminate={this.state.indeterminate}
+        />
+        <Image style={styles.icon} source={require("../assets/wheel.jpg")} />
+        <Progress.Bar
+          style={styles.progress}
+          progress={this.state.progress}
+          indeterminate={this.state.indeterminate}
+        />
         
         <TouchableOpacity onPress={() => navigate("Home")}>
-          <Image style={styles.icon} source={require("../assets/logo.png")} />
+          <Image style={styles.logo} source={require("../assets/logo.png")} />
         </TouchableOpacity>
       </View>
     );
@@ -31,14 +75,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
-  icon: {
+  logo: {
     width: 150,
     height: 150
   },
   profile: {
     width: "100%",
     height: "50%"
-  }
+  },
+  progress: {
+    margin: 10,
+  },
+  icon: {
+    width: 50,
+    height: 50
+  },
+
 });
 
 export default AchievementsScreen;
